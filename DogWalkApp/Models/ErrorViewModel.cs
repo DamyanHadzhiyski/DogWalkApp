9e7018f8248930abf8f7e429fc912f524e6212d6
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-03-17 20:48:32 +0200

    Add project files.

---
+++ DogWalkApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DogWalkApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
