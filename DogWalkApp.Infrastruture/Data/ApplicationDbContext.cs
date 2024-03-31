d16cccf6584e11ceeffd6b41539e941ae30488cf
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-03-31 21:41:08 +0300

    Created DogOwner and DogWalker entities

--- DogWalkApp.Infrastruture/Data/ApplicationDbContext.cs
+++ DogWalkApp.Infrastruture/Data/ApplicationDbContext.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DogWalkApp.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DogWalkApp.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Dog> Dogs { get; set; }
+
+        public DbSet<DogOwner> DogOwners { get; set; }
+
+        public DbSet<DogWalker> DogWalkers { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<WeightRange> WeightRanges { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ DogWalkApp.Infrastruture/Data/Models/DogOwner.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogWalkApp.Infrastructure.Data.Models
+{
+    public class DogOwner
+    {
+        [Key]
+        [Comment("Primary identifier of the dog owner")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Primary identifier of the user")]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+        [Required]
+        [Comment("List with the dogs the owner has")]
+        public List<Dog> Dogs { get; set; } = new List<Dog>();
+    }
+}
